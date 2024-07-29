32919ad55484f9f3e3005af33597078e103c2ac7
jpmarques2000 <jpmarques2000@hotmail.com>
2024-07-29 20:20:03 -0300

    Add project references

---
+++ Hackathon.HealthMed.Domain/Entities/Entity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hackathon.HealthMed.Domain.Entities
+{
+    public class Entity
+    {
+        public int Id { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+
+        public bool Enabled { get; set; }
+
+        public DateTime? LastModifiedDate { get; set; }
+    }
+}
