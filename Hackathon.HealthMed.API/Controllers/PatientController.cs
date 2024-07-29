987b269f6c9d64aaf70b6a82d4675019fb7ce573
Michel <michel.seve24@gmail.com>
2024-07-29 20:51:46 -0300

    Adicionando controlers

---
+++ Hackathon.HealthMed.API/Controllers/PatientController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Hackathon.HealthMed.API.Controllers
+{
+    public class PatientController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
